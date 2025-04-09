93decd4654627290463b3fa90057df21354ee950
duranginethmini <durangineth02@gmail.com>
2025-04-09 17:04:01 +0530

    CRUD implemented inside controller classes

--- src/main/java/com/example/tvgatewayassignment/controller/GatewayController.java
+++ src/main/java/com/example/tvgatewayassignment/controller/GatewayController.java
@@ -1,4 +1,43 @@
 package com.example.tvgatewayassignment.controller;
 
+import com.example.tvgatewayassignment.entity.Gateway;
+import com.example.tvgatewayassignment.service.GatewayService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/gateway")
 public class GatewayController {
+
+    @Autowired
+    private GatewayService gatewayService;
+
+    @GetMapping
+    public List<Gateway> getAll() {
+        return gatewayService.getAllGateway();
+    }
+
+    @GetMapping("/{id}")
+    public Gateway getOne(@PathVariable Long id) {
+        return gatewayService.getGateway(id);
+    }
+
+    @PostMapping
+    public Gateway create (@Valid @RequestBody Gateway gateway) {
+        return gatewayService.createGateway(gateway);
+    }
+
+    @PutMapping("/id")
+    public Gateway update (@PathVariable Long id, @Valid @RequestBody Gateway gateway) {
+         return gatewayService.updateGateway(id, gateway);
+    }
+
+    @DeleteMapping("/id")
+    public void delete (@PathVariable Long id) {
+        gatewayService.deleteGateway(id);
+    }
+
 }
