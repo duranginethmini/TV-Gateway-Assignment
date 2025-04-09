0e698e7ec6d3e1cff7f617bb866b56f0a99e60f1
duranginethmini <durangineth02@gmail.com>
2025-04-09 15:23:41 +0530

    entity classes created

--- src/main/java/com/example/tvgatewayassignment/entity/Gateway.java
+++ src/main/java/com/example/tvgatewayassignment/entity/Gateway.java
@@ -1,5 +1,33 @@
 package com.example.tvgatewayassignment.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Gateway {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String serialNumber;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$", message = "Invalid IPv4 address")
+    private String ipv4Address;
+
+    @OneToMany(mappedBy = "gateway", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<PeripheralDevice> peripheralDevices;
 }
--- src/main/java/com/example/tvgatewayassignment/entity/PeripheralDevice.java
+++ src/main/java/com/example/tvgatewayassignment/entity/PeripheralDevice.java
@@ -1,4 +1,40 @@
 package com.example.tvgatewayassignment.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PeripheralDevice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uid;
+
+    @NotBlank
+    private String vendor;
+
+    private LocalDateTime dateCreated;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "gateway_id", nullable = false)
+    private Gateway gateway;
+
+    @PrePersist
+    protected void onCreate() {
+        dateCreated = LocalDateTime.now();
+    }
+
+    public enum Status {
+        ONLINE, OFFLINE
+    }
 }
