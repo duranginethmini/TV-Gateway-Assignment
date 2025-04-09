669aa7a20e9aa11da62c0205af83895c20080983
duranginethmini <durangineth02@gmail.com>
2025-04-09 15:29:58 +0530

    Repository and exception classes implemented

--- src/main/java/com/example/tvgatewayassignment/repository/GatewayRepository.java
+++ src/main/java/com/example/tvgatewayassignment/repository/GatewayRepository.java
@@ -1,4 +1,7 @@
 package com.example.tvgatewayassignment.repository;
 
-public class GatewayRepository {
+import com.example.tvgatewayassignment.entity.Gateway;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GatewayRepository extends JpaRepository<Gateway, Long> {
 }
--- src/main/java/com/example/tvgatewayassignment/repository/PeripheralDeviceRepository.java
+++ src/main/java/com/example/tvgatewayassignment/repository/PeripheralDeviceRepository.java
@@ -1,4 +1,11 @@
 package com.example.tvgatewayassignment.repository;
 
-public class PeripheralDeviceRepository {
+import com.example.tvgatewayassignment.entity.PeripheralDevice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PeripheralDeviceRepository extends JpaRepository<PeripheralDevice, Long> {
+    List<PeripheralDevice> findByGatewayId(Long gatewayId);
 }
+
