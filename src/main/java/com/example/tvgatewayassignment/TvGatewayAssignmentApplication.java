b885ccd2bccf0b931f4fda4db7f96284572c0785
duranginethmini <durangineth02@gmail.com>
2025-04-09 14:39:31 +0530

    Initial commit: Project setup and structure

---
+++ src/main/java/com/example/tvgatewayassignment/TvGatewayAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.tvgatewayassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvGatewayAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvGatewayAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tvgatewayassignment/TvGatewayAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tvgatewayassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvGatewayAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
