1d69c3a467aba66f2afb4f421737365c0e621ba7
duranginethmini <durangineth02@gmail.com>
2025-04-09 17:03:10 +0530

    service classes implemented

--- src/main/java/com/example/tvgatewayassignment/service/GatewayService.java
+++ src/main/java/com/example/tvgatewayassignment/service/GatewayService.java
@@ -1,4 +1,43 @@
 package com.example.tvgatewayassignment.service;
 
+
+import com.example.tvgatewayassignment.entity.Gateway;
+import com.example.tvgatewayassignment.exception.ResourceNotFoundException;
+import com.example.tvgatewayassignment.repository.GatewayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GatewayService {
+
+    @Autowired
+    private GatewayRepository gatewayRepository;
+
+    public List<Gateway>getAllGateway() {
+        return gatewayRepository.findAll();
+    }
+
+    public Gateway getGateway(Long id) {
+        return gatewayRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Gateway not found with id" + id));
+    }
+
+    public Gateway createGateway (Gateway gateway){
+        return gatewayRepository.save(gateway);
+    }
+
+    public Gateway updateGateway(Long id , Gateway updated) {
+        Gateway gateway = getGateway(id);
+        gateway.setSerialNumber(updated.getSerialNumber());
+        gateway.setName(updated.getName());
+        gateway.setIpv4Address(updated.getIpv4Address());
+        return gatewayRepository.save(gateway);
+    }
+
+    public void deleteGateway(Long id) {
+        Gateway gateway = getGateway(id);
+        gatewayRepository.delete(gateway);
+    }
 }
--- src/main/java/com/example/tvgatewayassignment/service/PeripheralDeviceService.java
+++ src/main/java/com/example/tvgatewayassignment/service/PeripheralDeviceService.java
@@ -1,4 +1,54 @@
 package com.example.tvgatewayassignment.service;
 
+import com.example.tvgatewayassignment.entity.Gateway;
+import com.example.tvgatewayassignment.entity.PeripheralDevice;
+import com.example.tvgatewayassignment.exception.BadRequestException;
+import com.example.tvgatewayassignment.exception.ResourceNotFoundException;
+import com.example.tvgatewayassignment.repository.PeripheralDeviceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PeripheralDeviceService {
+
+    @Autowired
+    private PeripheralDeviceRepository peripheralDeviceRepository;
+
+    @Autowired
+    private GatewayService gatewayService;
+
+    public List<PeripheralDevice> getAllDevices(Long gatewayId){
+        gatewayService.getGateway(gatewayId);
+        return peripheralDeviceRepository.findByGatewayId(gatewayId);
+    }
+
+    public PeripheralDevice getDevice (Long gatewayId , Long uid) {
+        gatewayService.getGateway(gatewayId);
+        return peripheralDeviceRepository.findById(uid)
+                .filter(d -> d.getGateway().getId().equals(gatewayId))
+                .orElseThrow(() -> new ResourceNotFoundException("Device not found with ID" + uid));
+    }
+
+    public PeripheralDevice createDevice (Long gatewayId , PeripheralDevice peripheralDevice) {
+        Gateway gateway = gatewayService.getGateway(gatewayId);
+        if(gateway.getPeripheralDevices().size() >= 10) {
+            throw new BadRequestException("Gateway cannot have more than 10 devices");
+        }
+        peripheralDevice.setGateway(gateway);
+        return peripheralDeviceRepository.save(peripheralDevice);
+    }
+
+    public PeripheralDevice updateDevice (Long gatewayId , Long uid ,  PeripheralDevice updated) {
+        PeripheralDevice peripheralDevice =  getDevice(gatewayId,uid);
+        peripheralDevice.setVendor(updated.getVendor());
+        peripheralDevice.setStatus(updated.getStatus());
+        return peripheralDeviceRepository.save(peripheralDevice);
+    }
+
+    public void deleteDevice (Long gatewayId ,  Long uid) {
+         PeripheralDevice peripheralDevice = getDevice(gatewayId,uid);
+         peripheralDeviceRepository.delete(peripheralDevice);
+    }
 }
